7ca153db731d69d5dc62aea633e1b3df0fc1e6d5
Rawda Yasser <rawda.developer@gmail.com>
2023-04-01 20:30:31 +0200

    Feat: add frontend setup with material ui

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { Button } from "@mui/material"
+function App() {
+  return (
+    <>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
+    </>
+  );
+}
+
+export default App;
