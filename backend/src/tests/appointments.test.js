d2f51e6a6d9f71d7378667845f030ba45ca35835
Rawda Yasser <rawda.developer@gmail.com>
2023-04-01 04:37:33 +0200

    Test: add a test case for creating appointment

---
+++ backend/src/tests/appointments.test.js
@@ -0,0 +1,35 @@
+import app from "../server";
+import supertest from "supertest";
+import mongoose from "mongoose";
+import Appointment from "../models/appointment.model";
+import User from "../models/user.model";
+import { connect } from "../utils/dbConnection";
+import { user, createUser, getUserHeader } from "./authHeader";
+const request = supertest(app);
+beforeAll(async () => {
+  await connect();
+});
+afterAll(async () => {
+  await mongoose.connection.close();
+});
+afterEach(async () => {
+  await Appointment.deleteMany({});
+  await User.deleteMany({});
+});
+
+describe("POST /api/appointments", () => {
+  test("should create an appointment", async () => {
+    await createUser();
+    const authHeader = await getUserHeader();
+    const res = await request
+      .post(`/api/users/${user._id}/appointments`)
+      .set("Authorization", `Bearer ${authHeader}`)
+      .send({
+        petName: "Test",
+        aptNotes: "Test",
+        aptDate: "2022-04-01",
+      });
+    expect(res.status).toBe(200);
+    expect(res.body.message).toBe("Appointment created successfully");
+  });
+});
