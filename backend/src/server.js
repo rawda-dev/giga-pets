7082d324451ae0d11608b49b7257997c9271e542
Rawda Yasser <rawda.developer@gmail.com>
2023-04-01 03:20:23 +0200

    Feat: add user authentication endpoints login logout and user crud operations

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,8 +1,28 @@
 import express from "express";
+import mongoose from "mongoose";
+import { config } from "./config";
 const app = express();
+import authRouter from "./routes/auth.route";
+import usersRouter from "./routes/user.route";
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Giga Pets!");
 });
-app.listen(4000, () => {
-  console.log("Server running on port 4000!");
-});
+
+app.use("/api/auth", authRouter);
+app.use("/api/users", usersRouter);
+if (process.env.NODE_ENV !== "test") {
+  mongoose
+    .connect(config.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
+    .then(() => console.log("DB Connected!"))
+    .catch((err) => {
+      console.log(`DB Connection Error: ${err.message}`);
+    });
+  app.listen(config.PORT, () => {
+    console.log("Stream Mix listening on port 4000!!");
+  });
+}
+export default app;
